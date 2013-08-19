aab1b37717bbaa17587abb98b91e53def7fdc62d
monk-ee <lyndon.swan@flightcentre.com.au>
2013-08-19 14:45:20 +1000

    adjusting ready for splunk forge -removing my little helper files

--- bin/test/set_proxy_aws.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-export http_proxy="http://10.240.63.58:3128"
-export https_proxy="http://10.240.63.58:3128"
--- bin/test/set_proxy_syd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export http_proxy="http://syd-app-proxy.au.fcl.internal:3128"
-export https_proxy="http://syd-app-proxy.au.fcl.internal:3128"
