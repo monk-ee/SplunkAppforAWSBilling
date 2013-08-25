fbf065c72410d9b484c63d07358e4bc586b73b07
Lyndon Swan <magic.monkee.magic@gmail.com>
2013-08-25 10:48:39 +1000

    Update process_detailed_report.py
    ang again - damn auto format

--- bin/process_detailed_report.py
+++ bin/process_detailed_report.py
@@ -109,7 +109,7 @@ if processedFileExists == False:
             for col in row:
                 newrow += '"' + str(col) + '",'
                 newrow = newrow[:-1]
-                newrow += '\r\n'
+            newrow += '\r\n'
         processedCSVHandle.write(newrow)
         rownum += 1
     #dont shut it you idiot
