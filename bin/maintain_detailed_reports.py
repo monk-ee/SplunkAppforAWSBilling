d2f4fe91d7179f0c62b270e9a46face261283c88
Lyndon Swan <magic.monkee.magic@gmail.com>
2015-05-04 12:46:43 +1000

    quick and dirty maintain

--- bin/maintain_detailed_reports.py
+++ bin/maintain_detailed_reports.py
@@ -14,41 +14,109 @@ import yaml
 from splunk_utilities import *
 
 
-def main():
-    #setup error handling file
-    if not os.path.isfile(ERRORLOGFILE):
-        handleERRORLOGFILE = open(ERRORLOGFILE, 'w')
-    else:
-        handleERRORLOGFILE = open(ERRORLOGFILE, 'ab')
+import yaml
+import csv
+from splunk_utilities import *
+import splunk.clilib.cli_common
+import logging, logging.handlers
+import splunk
+from datetime import datetime
+import os
+import json
 
-    #load configuration
-    configurationStream = open(AWSCONFIGFILE, 'r')
-    configurationObject = yaml.load(configurationStream)
 
-    #now set detailed billing file
-    BILLINGREPORTZIPFILE = os.path.join(path, 'etc', 'apps', 'SplunkAppforAWSBilling', 'tmp',
-                                        str(configurationObject['s3']['account_number']) + 'detailed_billing.zip')
+class MaintainDetailedReports:
+    logger = ''
+    appname = 'SplunkAppforAWSBilling'
+    config = ''
+    splunk_home = ''
+    app_home = ''
+    report_date = ''
+    linenumber = 0
+    position = {}
 
-    #old file names
-    oldBillingFileName = str(configurationObject['s3'][
-        'account_number']) + "-aws-billing-detailed-line-items-with-resources-and-tags-" + subtract_one_month_datefilename() + ".csv"
-    oldBillingProcessedFileName = str(configurationObject['s3'][
-        'account_number']) + "-aws-billing-detailed-line-items-with-resources-and-tags-" + subtract_one_month_datefilename() + ".processed.csv"
+    def __init__(self):
+        """
+        Set some class variables for later use and kick off the processing chain
+        :return:
+        """
+        self.splunk_home = os.environ['SPLUNK_HOME']
+        self.app_home = os.path.join(self.splunk_home, 'etc', 'apps', self.appname)
+        self.set_date()
+        self.setup_logging()
+        self.setup_config()
+        self.process_files()
 
-    #now path it
-    BILLINGREPORTCSVOLDFILE = os.path.join(path, 'etc', 'apps', 'SplunkAppforAWSBilling', 'csv',oldBillingFileName)
-    BILLINGREPORTCSVOLDPROCESSEDFILE = os.path.join(path, 'etc', 'apps', 'SplunkAppforAWSBilling', 'csv',oldBillingProcessedFileName)
 
-    #now delete them try catch
-    try:
-        os.remove(BILLINGREPORTCSVOLDFILE)
-    except OSError:
-        pass
-    try:
-        os.remove(BILLINGREPORTCSVOLDPROCESSEDFILE)
-    except OSError:
-        pass
+    def set_date(self):
+        """
+        We calculate the month we are in just here.
+        :return:
+        """
+        dt = datetime.now()
+        self.report_date = dt.strftime("%Y-%m")
+
+    # Define the logging function
+    def setup_logging(self):
+        """
+        Just set up some splunky logging goodness.
+        :return:
+        """
+        self.logger = logging.getLogger('splunk.SplunkAppforAWSBilling')
+        LOGGING_DEFAULT_CONFIG_FILE = os.path.join(self.splunk_home, 'etc', 'log.cfg')
+        LOGGING_LOCAL_CONFIG_FILE = os.path.join(self.splunk_home, 'etc', 'log-local.cfg')
+        LOGGING_STANZA_NAME = 'python'
+        LOGGING_FILE_NAME = "SplunkAppforAWSBilling.log"
+        BASE_LOG_PATH = os.path.join('var', 'log', 'splunk')
+        LOGGING_FORMAT = "%(asctime)s %(levelname)-s\t%(module)s:%(lineno)d - %(message)s"
+        splunk_log_handler = logging.handlers.RotatingFileHandler(
+            os.path.join(self.splunk_home, BASE_LOG_PATH, LOGGING_FILE_NAME), mode='a')
+        splunk_log_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
+        self.logger.addHandler(splunk_log_handler)
+        splunk.setupSplunkLogger(
+            self.logger, LOGGING_DEFAULT_CONFIG_FILE, LOGGING_LOCAL_CONFIG_FILE, LOGGING_STANZA_NAME)
+
+    def setup_config(self):
+        """
+        This just loads the yaml that contains the configuration required to calculate the report file name(s)
+        :return:
+        """
+        try:
+            aws_key_file = os.path.join(self.app_home, 'local', 'aws.yaml')
+            config = open(aws_key_file, 'r')
+            self.config =  yaml.load(config)
+        except IOError, err:
+            self.logger.error("Failed to load configuration file aws.yaml: " + str(err))
+            raise SystemExit
+
+
+    def process_files(self):
+        """
+        Adding some support for multiple files and accounts - for aggregation
+        :return:
+        """
+        for key in self.config['accounts']:
+            self.process_file(key)
+
+    def process_file(self, key):
+        #now set detailed billing file
+        BILLINGREPORTZIPFILE = os.path.join(path, 'etc', 'apps', 'SplunkAppforAWSBilling', 'tmp',
+                                            str(key['account_number']) + 'detailed_billing.zip')
+
+        #old file names
+        oldBillingFileName = str(key[
+            'account_number']) + "-aws-billing-detailed-line-items-with-resources-and-tags-" + subtract_one_month_datefilename() + ".csv"
+
+        #now path it
+        BILLINGREPORTCSVOLDFILE = os.path.join(path, 'etc', 'apps', 'SplunkAppforAWSBilling', 'csv',oldBillingFileName)
+
+        #now delete them try catch
+        try:
+            os.remove(BILLINGREPORTCSVOLDFILE)
+        except OSError:
+            pass
+
 
 if __name__ == "__main__":
-    main()
+    mdr = MaintainDetailedReports()
 
