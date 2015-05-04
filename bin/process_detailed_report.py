62079a18f0ed4f4655773bad8c414de261eba400
Lyndon Swan <magic.monkee.magic@gmail.com>
2015-05-04 10:26:55 +1000

    ok so this should kind of pass tests - but positional stuff not implemented yet

--- bin/process_detailed_report.py
+++ bin/process_detailed_report.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""process_detailed_bill.py: Init for this module."""
+"""process_detailed_bill.py: Init for this module.
+
+    The purpose of this script is to parse the csv report file into a stream that can be consumed by splunk. It has a
+    concept of file position stored in a yaml position file. It uses this file to determine what parts of the file it
+    has processed.
+
+    This is due to the file changing in the S3 bucket with additional LineItems.
+
+    Note: a merror is a recoverable error, it may not be critical to the functioning or parsing of the script
+
+    We use the record type to determine position within the csv also whether an item is billable:
+    RecordType
+      - LineItem
+      - Rounding
+      - InvoiceTotal
+      - AccountTotal
+      - StatementTotal
+
+    This is stored in a yaml file like so:
+
+"""
 
 __author__ = "monkee"
 __license__ = "GPLv3.0"
@@ -15,6 +35,8 @@ import yaml
 import csv
 from splunk_utilities import *
 from datetime import datetime
+import os
+
 
 class ProcessDetailedReport:
     logger = ''
@@ -24,8 +46,13 @@ class ProcessDetailedReport:
     app_home = ''
     report_date = ''
     linenumber = 0
+    position = ''
 
     def __init__(self):
+        """
+        Set some class variables for later use and kick off the processing chain
+        :return:
+        """
         self.splunk_home = os.environ['SPLUNK_HOME']
         self.app_home = os.path.join(self.splunk_home, 'etc', 'apps', self.appname)
         self.set_date()
@@ -35,11 +62,19 @@ class ProcessDetailedReport:
 
 
     def set_date(self):
+        """
+        We calculate the month we are in just here.
+        :return:
+        """
         dt = datetime.now()
         self.report_date = dt.strftime("%Y-%m")
 
     # Define the logging function
     def setup_logging(self):
+        """
+        Just set up some splunky logging goodness.
+        :return:
+        """
         self.logger = logging.getLogger('splunk.SplunkAppforAWSBilling')
         LOGGING_DEFAULT_CONFIG_FILE = os.path.join(self.splunk_home, 'etc', 'log.cfg')
         LOGGING_LOCAL_CONFIG_FILE = os.path.join(self.splunk_home, 'etc', 'log-local.cfg')
@@ -47,12 +82,18 @@ class ProcessDetailedReport:
         LOGGING_FILE_NAME = "SplunkAppforAWSBilling.log"
         BASE_LOG_PATH = os.path.join('var', 'log', 'splunk')
         LOGGING_FORMAT = "%(asctime)s %(levelname)-s\t%(module)s:%(lineno)d - %(message)s"
-        splunk_log_handler = logging.handlers.RotatingFileHandler(os.path.join(self.splunk_home, BASE_LOG_PATH, LOGGING_FILE_NAME), mode='a')
+        splunk_log_handler = logging.handlers.RotatingFileHandler(
+            os.path.join(self.splunk_home, BASE_LOG_PATH, LOGGING_FILE_NAME), mode='a')
         splunk_log_handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
         self.logger.addHandler(splunk_log_handler)
-        splunk.setupSplunkLogger(self.logger, LOGGING_DEFAULT_CONFIG_FILE, LOGGING_LOCAL_CONFIG_FILE, LOGGING_STANZA_NAME)
+        splunk.setupSplunkLogger(
+            self.logger, LOGGING_DEFAULT_CONFIG_FILE, LOGGING_LOCAL_CONFIG_FILE, LOGGING_STANZA_NAME)
 
     def setup_config(self):
+        """
+        This just loads the yaml that contains the configuration required to calculate the report file name(s)
+        :return:
+        """
         try:
             aws_key_file = os.path.join(self.app_home, 'local', 'aws.yaml')
             config = open(aws_key_file, 'r')
@@ -62,26 +103,48 @@ class ProcessDetailedReport:
             raise SystemExit
 
     def process_files(self):
+        """
+        Adding some support for multiple files and accounts - for aggregation
+        :return:
+        """
         for key in self.config['accounts']:
             self.fetch_file(key)
 
     def process_file(self, key):
+        """
+        Just a layer to clarify the processing chain for multiple accounts
+        :param key:
+        :return:
+        """
         s3_billing_report = str(key[
             'account_number']) + "-aws-billing-detailed-line-items-with-resources-and-tags-" + self.report_date + ".csv"
-        try:
-            pos_file = "{0}.pos".format(s3_billing_report)
-            abs_file_path = os.path.join(self.app_home, 'local', pos_file)
-            f = open(abs_file_path, 'r')
-            d = csv.DictReader(f, quoting=csv.QUOTE_ALL)
-            for row in d:
-                if row['object'] == s3_billing_report:
-                    self.linenumber = row['latest']
-        except IOError, err:
-            self.logger.error("MERROR - Position File may not exist: " + str(err))
-            return
+        self.load_position(s3_billing_report)
         self.parse(s3_billing_report)
 
+
+    def load_position(self, s3_billing_report):
+        """
+        :param s3_billing_report: this is the calculated name of the report file based on account number and month/year
+        :return:
+        """
+        try:
+            pos_file = "{0}.yaml".format(s3_billing_report)
+            abs_file_path = os.path.join(self.app_home, 'local', pos_file)
+            file = open(abs_file_path, 'r')
+            self.position = yaml.load(file)
+        except IOError, err:
+            #self.logger.error("MERROR - Position File may not exist: " + str(err))
+            #we dont care about this really - will happen first time a file is seen
+            pass
+        return
+
     def parse(self, report):
+        """
+        In this function we are going to process the file, we will do this based on the positional elements of a
+        previous run. It will load the file each time but hopefully not waste too much time or resources doing this.
+        :param report: this is the calculated name of the report file based on account number and month/year
+        :return:
+        """
         report_path = os.path.join(self.app_home, 'csv', report)
         try:
             ifile = open(report_path, 'rb')
@@ -92,119 +155,49 @@ class ProcessDetailedReport:
             self.logger.error("MERROR - Report File may not exist: " + str(err))
             return
 
-        #ok read this file
+        #ok read the report file in
         reader = csv.reader(ifile)
 
-        #processnewcsv
-        processedCSVFileName = str(configurationObject['s3'][
-            'account_number']) + "-aws-billing-detailed-line-items-with-resources-and-tags-" + datefilename() + ".processed.csv"
-        BILLINGREPORTZIPDIRPROCESSEDCSV = os.path.join(path, 'etc', 'apps', 'SplunkAppforAWSBilling', 'csv',
-                                                       processedCSVFileName)
-
-        #more complex file situation - the processed file already exists
-        #we only want to append the file differences and throw away the header row
-        processedFileExists = True
-        try:
-            with open(BILLINGREPORTZIPDIRPROCESSEDCSV):
-                pass
-        except IOError:
-            #ok now we need to take another path
-            processedFileExists = False
-
-        if processedFileExists == False:
-            #process new csv
-            print "processing a totally new file"
-            processedCSVHandle = open(BILLINGREPORTZIPDIRPROCESSEDCSV, 'wb')
-
-            # go loop go
-            rownum = 0
-            for row in reader:
-                newrow = ""
-                if rownum == 0:
-                    #format the header row
-                    newrow += '"Timestamp",'
-                    newrow += '"AccountNumber",'
-                    for col in row:
-                        newrow += '"' + str(col) + '",'
-                    newrow = newrow[:-1]
-                    newrow += '\r\n'
-                elif row[8] == "":
-                     #if the subscriptionid is blank - i dont want that record
-                    rownum += 1
-                    continue
-                else:
-                    if row[15] != "":
-                        try:
-                            rowdate = datetime.strptime(row[15], '%Y-%m-%d %H:%M:%S')
-                            newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
-                        except Exception:
-                            sys.exc_clear()
-                            falsedate = datefilename() + "-01 00:00:00"
-                            rowdate = datetime.strptime(falsedate, '%Y-%m-%d %H:%M:%S')
-                            newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
-                    else:
+        # we looooooooop here and do something re-org so splunk understands it better (that's the theory ;))
+        # ps you can use csv.line_number instead of counting - it's better for the environment
+        for row in reader:
+            newrow = ""
+            if rownum == 0:
+                #format the header row
+                newrow += '"Timestamp",'
+                newrow += '"AccountNumber",'
+                for col in row:
+                    newrow += '"' + str(col) + '",'
+                newrow = newrow[:-1]
+                newrow += '\r\n'
+            elif row[8] == "":
+                 #if the subscriptionid is blank - i dont want that record
+                rownum += 1
+                continue
+            else:
+                if row[15] != "":
+                    try:
+                        rowdate = datetime.strptime(row[15], '%Y-%m-%d %H:%M:%S')
+                        newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
+                    except Exception:
+                        sys.exc_clear()
                         falsedate = datefilename() + "-01 00:00:00"
                         rowdate = datetime.strptime(falsedate, '%Y-%m-%d %H:%M:%S')
                         newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
-
-                    newrow = str(newdate) + ', '
-                    newrow += str(configurationObject['s3']['account_number']) + ', '
-                    for col in row:
-                        newrow += '"' + str(col) + '",'
-                    newrow = newrow[:-1]
-                    newrow += '\r\n'
-                processedCSVHandle.write(newrow)
-                rownum += 1
-            #dont shut it you idiot
-            processedCSVHandle.close()
-        else:
-            #process old csv
-            #ok at this point we need to count the row total difference
-            processedCSVRowCount = sum(1 for row in csv.reader(open(BILLINGREPORTZIPDIRPROCESSEDCSV)))
-
-            #process old csv
-            processedCSVHandle = open(BILLINGREPORTZIPDIRPROCESSEDCSV, 'ab')
-
-            # go loop go
-            rownum = 0
-            for row in reader:
-                newrow = ""
-                #if the subscriptionid is blank - i dont want that record
-                if row[8] == "":
-                    rownum += 1
-                    continue
-                    #format the header row
-                if rownum < processedCSVRowCount:
-                    #do nothing
-                    rownum += 1
-                    continue
                 else:
-                    if row[15] != "":
-                        try:
-                            rowdate = datetime.strptime(row[15], '%Y-%m-%d %H:%M:%S')
-                            newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
-                        except Exception:
-                            sys.exc_clear()
-                            falsedate = datefilename() + "-01 00:00:00"
-                            rowdate = datetime.strptime(falsedate, '%Y-%m-%d %H:%M:%S')
-                            newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
-                    else:
-                        falsedate = datefilename() + "-01 00:00:00"
-                        rowdate = datetime.strptime(falsedate, '%Y-%m-%d %H:%M:%S')
-                        newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
+                    falsedate = datefilename() + "-01 00:00:00"
+                    rowdate = datetime.strptime(falsedate, '%Y-%m-%d %H:%M:%S')
+                    newdate = rowdate.strftime("%b %d %Y %I:%M:%S %p %z")
 
-                    newrow = str(newdate) + ', '
-                    newrow += str(configurationObject['s3']['account_number']) + ', '
-                    for col in row:
-                        newrow += '"' + str(col) + '",'
-                    newrow = newrow[:-1]
-                    newrow += '\r\n'
-                processedCSVHandle.write(newrow)
-                rownum += 1
-            #dont shut it you idiot
-            processedCSVHandle.close()
-        #close that pesky open file
-        ifile.close()
+                newrow = str(newdate) + ', '
+                newrow += str(configurationObject['s3']['account_number']) + ', '
+                for col in row:
+                    newrow += '"' + str(col) + '",'
+                newrow = newrow[:-1]
+                newrow += '\r\n'
+            processedCSVHandle.write(newrow)
+            rownum += 1
+
 
 if __name__ == "__main__":
-    fdr = FetchDetailedReport()
+    pdr = ProcessDetailedReport()
